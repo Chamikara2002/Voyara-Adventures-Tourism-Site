3a401d9e792afff25146b6bb65b2244e352b1f7e
Chamikara2002 <vilochanabandara25@gmail.com>
2026-05-26 20:21:35 +0530

    Fixed Some Alingments and Develop About page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,35 @@
-//import { Link } from "react-router-dom";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
+  useNavigate
 } from "react-router-dom";
+import { useEffect } from "react";
 
 // Pages Imports
 import Home from "./Pages/home-page";
+import About from "./Pages/about-page";
 import Service from "./Pages/service-page";
 import Packages from "./Pages/packages-page";
 import Tour from "./Pages/tours-page";
 import Navbar from "./components/Navigation/navbar";
 import Footer from "./components/Footer/footer";
 
-
-
-
 function AppContent() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const navigationEntries = performance.getEntriesByType("navigation");
+    const isReload = navigationEntries.length > 0 && navigationEntries[0].type === "reload";
+
+    const isFirstLoadInSession = !sessionStorage.getItem("session_started");
+
+    if (isReload || isFirstLoadInSession) {
+      sessionStorage.setItem("session_started", "true");
+      navigate("/");
+    }
+  }, [navigate]);
 
   return (
     <>
@@ -25,7 +37,7 @@ function AppContent() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Service />} />
-        {/* <Route path="/about" element={<About />} />*/} 
+        <Route path="/about" element={<About />} /> 
         <Route path="/packages" element={<Packages />} />
         <Route path="/tours" element={<Tour />} />
         <Route path="*" element={<Navigate to="/" />} />
